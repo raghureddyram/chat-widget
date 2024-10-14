b05a30cbf91ce629376917a7b14915f6613cc4eb
Raghu Reddy <raghureddy@Raghus-MacBook-Pro.local>
2024-10-13 23:10:44 -0700

    Handle logout

--- widget-frontend/src/app/page.tsx
+++ widget-frontend/src/app/page.tsx
@@ -1,10 +1,17 @@
+'use client'
 import React from 'react';
 import ChatboxWithAuth from "./components/ChatbotWithAuth";
 
 const Home: React.FC = () => {
+  const handleLogout = () => {
+    window.location.href = "http://localhost:8000/logout"
+    return
+  }
+
   return (
     <div>
       <h1>Welcome to the Homepage</h1>
+      <button onClick={() => (handleLogout())}>Logout</button>
       <ChatboxWithAuth />  {/* This will display the chatbox if the user is logged in */}
     </div>
   );
